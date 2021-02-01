628d62cf6304adc548e8dad96d21d94f52b7baf2
Oleh Kulykov <oleh.kulykov@boerse-go.de>
2021-02-01 09:06:00 +0100

    Release 1.0.4

--- Package.swift
+++ Package.swift
@@ -13,10 +13,10 @@ let package = Package(
         .target(name: "libplzma",
                 path: "src",
                 cSettings: [
-                    .define("LIBPLZMA_VERSION_BUILD", to: "205")
+                    .define("LIBPLZMA_VERSION_BUILD", to: "216")
                 ],
                 cxxSettings: [
-                    .define("LIBPLZMA_VERSION_BUILD", to: "205")
+                    .define("LIBPLZMA_VERSION_BUILD", to: "216")
         ]),
         .target(name: "PLzmaSDK",
                 dependencies: [
